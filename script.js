033ab66be753f432eb61e60d12b850ccf5dfe0cc
arion2001 <arionnagappen@gmail.com>
2023-10-09 18:54:25 +0200

    menu icon rotates when clicked

--- script.js
+++ script.js
@@ -4,9 +4,23 @@ const getElement = (selector) => {
     return element;
 }
 
+
+// ROTATE MENU ICON ON SMALL SCREENS
 const menuButton = getElement('.menu-btn');
 const menuItems = getElement('.nav-menu');
+let rotateAngle = 0;
 
 menuButton.addEventListener('click', function(){
-    menuItems.classList.toggle('show-items');
-})
+
+    if (rotateAngle != 90) {
+        rotateAngle += 90;
+
+        menuButton.style.transform = `rotate(${rotateAngle}deg)`
+
+    } else {
+        menuButton.style.transform = `rotate(0deg)`
+        rotateAngle = 0;
+    }
+
+    menuItems.classList.toggle('show-items');    
+});
